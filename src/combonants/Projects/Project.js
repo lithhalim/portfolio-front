47021cb0b7ae1596d804f7c33e5b13ee4215ba7c
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-10-16 15:26:46 +0300

    lith

--- src/combonants/Projects/Project.js
+++ src/combonants/Projects/Project.js
@@ -16,23 +16,25 @@ import fotballImage from "../../assest/ProjectImage/imageFootball.png";
 import syarahimage from "../../assest/ProjectImage/syaraheccomers.png";
 import sweetproject from "../../assest/ProjectImage/sweeteccomers.png";
 //-------------------------------------------vedio section ---------------------------------------------//
-import vedio1 from "../../assest/vedio/videoplayback.mp4";
-import EcommersVedio from "../../assest/vedio/eccomers.mp4";
-import socialvedio from "../../assest/vedio/social-vedio.mp4";
-import football from "../../assest/vedio/football.mp4";
+import syarahvedio from "../../assest/vedio/syarah.mp4";
+import sweetvedio from "../../assest/vedio/sweet.mp4";
+import footballvedio from "../../assest/vedio/football.mp4";
+import eccomersVedio from "../../assest/vedio/eccomers.mp4";
+import facebookvedio from "../../assest/vedio/facebook.mp4";
+import othervedio from "../../assest/vedio/othervedio.mp4";
 
 
 
 import { useState } from 'react';
 
 let dataUse=[
-{github:"https://github.com/lithhalim/sweet-frontEnd",netlify:"https://sweet-eccomers.netlify.app/",tutorial:football,image:sweetproject,type:"bigProject"},
-{github:"https://github.com/lithhalim/syarah-frontend",netlify:"https://syarah-eccomers.netlify.app/",tutorial:football,image:syarahimage,type:"bigProject"},
-{github:"https://github.com/lithhalim/face-front",netlify:"https://634be91b47ed83696ddf09fc--brilliant-fairy-2313ac.netlify.app/",tutorial:socialvedio,image:socialimage,type:"bigProject"},
-{github:"https://github.com/lithhalim/eccomers-front",netlify:"https://eccomers-application-lith.netlify.app/",tutorial:EcommersVedio,image:eccomersImage,type:"bigProject"},
-{github:"https://github.com/lithhalim/Football_frontEnd",netlify:"https://app.netlify.com/sites/footballwebsite11/overview",tutorial:football,image:fotballImage,type:"bigProject"},
-{github:"https://github.com/lithhalim/store-frontend",netlify:"https://cozy-fenglisu-af2346.netlify.app/",tutorial:vedio1,image:storeimage,type:"smallProject"},
-{github:"https://github.com/lithhalim/Todo-List",netlify:"https://6321b90a50be9b0aec67c4bd--warm-sprinkles-1d72ae.netlify.app/",tutorial:vedio1,image:todoimage,type:"smallProject"},
+{github:"https://github.com/lithhalim/sweet-frontEnd",netlify:"https://sweet-eccomers.netlify.app/",tutorial:sweetvedio,image:sweetproject,type:"bigProject"},
+{github:"https://github.com/lithhalim/syarah-frontend",netlify:"https://syarah-eccomers.netlify.app/",tutorial:syarahvedio,image:syarahimage,type:"bigProject"},
+{github:"https://github.com/lithhalim/face-front",netlify:"https://634be91b47ed83696ddf09fc--brilliant-fairy-2313ac.netlify.app/",tutorial:facebookvedio,image:socialimage,type:"bigProject"},
+{github:"https://github.com/lithhalim/eccomers-front",netlify:"https://eccomers-application-lith.netlify.app/",tutorial:eccomersVedio,image:eccomersImage,type:"bigProject"},
+{github:"https://github.com/lithhalim/Football_frontEnd",netlify:"https://app.netlify.com/sites/footballwebsite11/overview",tutorial:footballvedio,image:fotballImage,type:"bigProject"},
+{github:"https://github.com/lithhalim/store-frontend",netlify:"https://cozy-fenglisu-af2346.netlify.app/",tutorial:othervedio,image:storeimage,type:"smallProject"},
+{github:"https://github.com/lithhalim/Todo-List",netlify:"https://6321b90a50be9b0aec67c4bd--warm-sprinkles-1d72ae.netlify.app/",tutorial:othervedio,image:todoimage,type:"smallProject"},
 ]
 
 
